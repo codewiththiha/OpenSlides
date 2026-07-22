c22f8bf65273882367247c182b41237325731f26
codewiththiha <codewiththiha@users.noreply.github.com>
2026-07-22 10:15:16 +0000

    fix: use theme-aware caret and resilient desktop routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./components/Dashboard";
 import { Editor } from "./components/Editor";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/editor/:projectId" element={<Editor />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
